ce3ccbb4d1afaa293269385f50a14d5a2a303434
sowmika <sowmika148@gmail.com>
2019-02-18 15:46:01 +0530

    Sneha Bagri, Sowmika | MOBN-284 | Convert repo to npm package

--- src/index.js
+++ src/index.js
@@ -1,40 +1,5 @@
 import { parseContent } from "./parsers/parser";
-import fs from "fs";
-import request from "request";
 
-function writeResultToFile(content) {
-    let parsedContent = parseContent(content);
-
-    fs.writeFile("result_form_condition.json", JSON.stringify(parsedContent),
-        (err) => {
-            if (err) {
-                throw err;
-            }
-        });
-}
-
-function init() {
-    const args = process.argv.slice(2);
-    const filePath = args[ 0 ];
-    const isUrl = (args[ 1 ] === "true");
-
-    if (isUrl) {
-        request({ url: filePath, "rejectUnauthorized": false }, ("response", (err, response, body) => {
-            writeResultToFile(body);
-            if (err) {
-                throw err;
-            }
-        }));
-    } else {
-        fs.readFile(filePath, "UTF-8", (err, content) => {
-            writeResultToFile(content);
-            if (err) {
-                throw err;
-            }
-        });
-
-    }
-}
-
-
-init();
+export default {
+    parseContent
+};
